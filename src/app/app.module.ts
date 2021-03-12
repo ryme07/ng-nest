a895f0384b30f0ad12dbabc0153dab3ddc99252e
ryme07 <remydelrieu@hotmail.fr>
2021-03-12 12:40:06 +0100

    feat: create two modules public & secure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    MenuComponent
-  ],
+    AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/secure/secure.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { SecureComponent } from './secure.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    MenuComponent,
+    SecureComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SecureComponent
+  ]
+})
+export class SecureModule { }
