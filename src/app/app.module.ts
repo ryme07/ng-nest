496320b9ee97402c7836eba5b338a657e5d50dc1
ryme07 <remydelrieu@hotmail.fr>
2021-03-12 13:55:09 +0100

    feat: try register form v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     SecureModule,
-    PublicModule
+    PublicModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,4 +1,7 @@
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,20 +15,20 @@ export class RegisterComponent implements OnInit {
   password = '';
   passwordConfirm = '';
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   submit(): void {
-    console.log({
 
+    this.http.post(`${environment.api}/register`, {
       last_name: this.lastName,
       first_name: this.firstName,
       email: this.email,
       password: this.password,
       password_confirm: this.passwordConfirm
-    })
+    }).subscribe(() => this.router.navigate(['/login']))
   }
 
 }
